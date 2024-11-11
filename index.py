1001c7f75ecfd96303e7b42e0d0ad08a5e786fc0
Yohei <y.meguro.2008@gmail.com>
2024-11-11 12:42:35 +0900

    feat: simpleの完成

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+parser = StrOutputParser()
 
 model = ChatOpenAI(model="gpt-4")
 
-messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
-]
+# messages = [
+#     SystemMessage(content="Translate the following from English into Italian"),
+#     HumanMessage(content="hi!"),
+# ]
 
-model.invoke(messages)
+# result = model.invoke(messages)
+# parser.invoke(result)
+
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+chain = prompt_template | model | parser
+chain.invoke({"language": "japanese", "text": "hi"})
