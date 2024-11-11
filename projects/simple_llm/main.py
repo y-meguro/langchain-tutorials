479493dc222e5479b1b035a4237a5269a7fb3af0
Yohei <y.meguro.2008@gmail.com>
2024-11-11 13:13:21 +0900

    fix

--- projects/simple_llm/main.py
+++ projects/simple_llm/main.py
@@ -1,10 +1,15 @@
+import os
 from dotenv import load_dotenv
+from pathlib import Path
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
+
+print(os.getenv('LANGCHAIN_PROJECT'))
+print(os.getenv('LANGCHAIN_ENDPOINT'))
 
 parser = StrOutputParser()
 
